b818e48cd32720c731b6f70574ccb1482f5802b1
testoviphp <testoviphp@gmail.com>
2022-04-02 00:00:03 +0200

    2

--- src/InputText.vue
+++ src/InputText.vue
@@ -1,5 +1,5 @@
 <template>
-<h1 class="bg-red shadow-md px-8 pt-6 pb-8 mb-4">welcome</h1>
+<h1 class="bg-blue-600 shadow-md px-8 pt-6 pb-8 mb-4">welcome</h1>
 <input type="text">dadada
 {{ title }}
 </template>
